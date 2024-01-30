20a2ef40ea2fc018e24c504a223363b033867bf7
CodedClovey <abhinavmanoj222@gmail.com>
2024-01-30 17:36:17 +0530

    morgan

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     { 
       "id": 1,
@@ -24,8 +26,63 @@ let notes = [
     }
 ]
 
+app.get('/info', (request, response) => {
+  response.send(`<p>phonebook has info about ${notes.length} people </p> 
+    <p>${Date()}</p>`)
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(notes)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+})
+
+const generateId = () => {
+    return Math.floor(Math.random()*1000)
+}
+  
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    const checklist = notes.find(guy => guy.name === body.name)
+
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+
+    if (checklist) {
+        console.log(checklist)
+        return response.status(400).json({ 
+            error: 'name is already registered' 
+          })
+    }
+  
+    const note = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    }
+    console.log(checklist)
+    console.log(body.name)
+
+    notes = notes.concat(note)
+    response.json(note)
 })
 
 const PORT = 3001
